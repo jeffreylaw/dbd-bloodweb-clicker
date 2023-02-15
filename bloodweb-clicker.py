34587217ab1461f5f98e70c9b4e04056db3fdded
Jeffrey Law <jylday56@gmail.com>
2023-02-15 11:46:13 -0800

    Add resume, pause keys, update exit key, refactor common code into funcs

--- bloodweb-clicker.py
+++ bloodweb-clicker.py
@@ -5,7 +5,6 @@ import yaml
 import numpy as np
 import cv2
 import pydirectinput
-import pyautogui
 from pynput import keyboard
 from PIL import ImageGrab
 from ctypes import wintypes, windll, create_unicode_buffer, pointer
@@ -19,10 +18,15 @@ logger = logging.getLogger(__name__)
 class BloodwebClicker:
 
     def __init__(self) -> None:
-        self.top_l = (-1, -1)
-        self.bottom_r = (-1, -1)
+        self.topleft_x = -1
+        self.topleft_y = -1
+        self.bottomright_x = -1
+        self.bottomright_y = -1
+
         self.height_res = None
         self.width_res = None
+        self.paused = False
+
         self.get_monitor_res()
         listener = keyboard.Listener(on_press=self.keyboard_listener)
         listener.start()
@@ -31,44 +35,85 @@ class BloodwebClicker:
     def click_and_hold(self, x, y, duration) -> None:
         pydirectinput.moveTo(x, y)
         pydirectinput.mouseDown(x, y)
-        pydirectinput.moveTo(int(((self.bottom_r[0] - self.top_l[0]) / 2)) + self.top_l[0], self.top_l[1]+10) # type: ignore
+        pydirectinput.moveTo(int(((self.bottomright_x[0] - self.topleft_x[0]) / 2)) + self.topleft_x[0], self.topleft_y[1]+10)
         time.sleep(duration)
-        pydirectinput.mouseUp(int(((self.bottom_r[0] - self.top_l[0]) / 2)) + self.top_l[0], self.top_l[1]+10) # type: ignore
+        pydirectinput.mouseUp(int(((self.bottomright_x[0] - self.topleft_x[0]) / 2)) + self.topleft_x[0], self.topleft_y[1]+10)
         pydirectinput.click()
+
+    """ Get foreground window handle """
+    def get_foreground_win_handle(self):
+        foreground_window_handle = windll.user32.GetForegroundWindow()
+        return foreground_window_handle
+
+    """ Get foreground window title """
+    def get_foreground_win_title(self, hdl) -> str:
+        window_title_len = windll.user32.GetWindowTextLengthW(hdl)
+        buffer = create_unicode_buffer(window_title_len + 1)
+        windll.user32.GetWindowTextW(hdl, buffer, window_title_len + 1)
+
+        if not buffer.value:
+            return ""
+
+        return buffer.value.strip()
+
+    """ Set x,y coords of upper-left and bottom-right corners of DBD window """
+    def set_coords_dbd_window(self, hdl) -> None:
+        rect = wintypes.RECT()
+        windll.user32.GetWindowRect(hdl, pointer(rect))      
+
+        self.topleft_x = rect.left
+        self.topleft_y = rect.top
+        self.bottomright_x = rect.right
+        self.bottomright_y = rect.bottom
+
+        logger.info(f"Setting -> Topleft: {self.topleft_x},{self.topleft_y}. Bottomright: {self.bottomright_x},{self.bottomright_y}")
 
     """ Check if dbd is in foreground and set top left, bottom right coordinates """
     def check_for_dbd(self) -> bool:
-        foreground_window_handle = windll.user32.GetForegroundWindow()
-        window_title_len = windll.user32.GetWindowTextLengthW(foreground_window_handle)
-        buffer = create_unicode_buffer(window_title_len + 1)
-        windll.user32.GetWindowTextW(foreground_window_handle, buffer, window_title_len + 1)
+        foreground_hdl = self.get_foreground_win_handle()
+        foreground_win_title = self.get_foreground_win_title(foreground_hdl)
 
-        if not buffer.value and buffer.value.strip() != "DeadByDaylight":
-            return False
-            
-        rect = wintypes.RECT()
-        windll.user32.GetWindowRect(foreground_window_handle, pointer(rect))        
-
-        if rect.left < 0: # type: ignore    
+        if foreground_win_title != "DeadByDaylight":
             return False
 
-        self.top_l = (rect.left, rect.top)
-        self.bottom_r = (rect.right, rect.bottom)
-        # print(f"Topleft: {rect.left},{rect.top}. Bottomright: {rect.right},{rect.bottom}")
+        if self.topleft_x < 0 or self.topleft_y < 0:   
+            return False
+
+        self.set_coords_dbd_window(foreground_hdl)
         return True
 
-
     """ Listener for keyboard presses
-        - ESC key to exit script
+        - F1 to resume script
+        - F2 to pause script
+        - F4 to exit script
     """
     def keyboard_listener(self, key) -> None:
-        if key == keyboard.Key.esc:
-            logger.info("Exiting script")
-            os.kill(os.getpid(), signal.SIGTERM)
+        match key:
+            case keyboard.Key.f1:
+                if self.paused:
+                    logger.info("Resuming script")
+                    self.paused = False
+
+            case keyboard.Key.f2:
+                if not self.paused:
+                    logger.info("Pausing script")
+                    self.paused = True
+
+            case keyboard.Key.f4:
+                hdl = self.get_foreground_win_handle()
+                foreground_win_title = self.get_foreground_win_title(hdl)
+                if foreground_win_title == "DeadByDaylight":
+                    pydirectinput.mouseUp()
+                    
+                logger.info("Exiting script")
+                os.kill(os.getpid(), signal.SIGTERM)
 
     """ Start screenshotting screen, look for circles using OpenCV, and click them """
     def start(self) -> None:
         while True:
+            if self.paused:
+                continue
+
             try:
                 if not self.check_for_dbd():
                     time.sleep(0.5)
@@ -84,7 +129,7 @@ class BloodwebClicker:
                 if circles is not None:
                     detected_circles = np.uint16(np.around(circles))
                     for (x, y, r) in detected_circles[0, :]:
-                        if self.top_l[0] <= x <= self.bottom_r[0] and self.top_l[1] <= y <= self.bottom_r[1]:
+                        if self.topleft_x[0] <= x <= self.bottomright_x[0] and self.topleft_y[1] <= y <= self.bottomright_y[1]:
                             self.click_and_hold(x, y, 1.1)
             except Exception as exc:
                 logging.exception("Exception occured:")
